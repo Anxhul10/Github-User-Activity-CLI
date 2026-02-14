ee271457d783c8fe1beb305ad6526167e6da12ea
Anxhul10 <anxhul10@gmail.com>
2026-02-15 03:22:45 +0530

    chore: structure the response from API

--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
  "fmt"
+ "io"
  "github.com/spf13/cobra"
  "strings"
+ "net/http"
+  "encoding/json"
 )
 
-func runner(cmd *cobra.Command, args []string) {
-  var text string
-  if len(args) > 0 {
-    text = strings.Join(args, " ")
-	} else {
-    fmt.Println("please pass username!!");
-    return
-  }
-  fmt.Println(text)
+type Event struct {
+    ID   string `json:"id"`
+    Type string `json:"type"`
+    Repo struct {
+        Name string `json:"name"`
+    } `json:"repo"`
+    Payload struct {
+      Action string `json: "action"`
+    } `json: payload`
+    CreatedAt string `json:"created_at"`
 }
 
 var rootCmd = &cobra.Command{
@@ -28,6 +32,40 @@ func Execute() {
  cobra.CheckErr(rootCmd.Execute())
 }
 
+func runner(cmd *cobra.Command, args []string) {
+  var username string
+  if len(args) > 0 {
+    username = strings.Join(args, " ")
+	} else {
+    fmt.Println("please pass username!!");
+    return
+  }
+  logResponse(username)
+}
+
+func logResponse(username string) {
+  client := &http.Client{}
+  req, err := http.NewRequest("GET", "https://api.github.com/users/"+username+"/events/public", nil)
+
+  if err != nil {
+    fmt.Println("Request failed: %s", err)
+    return
+  }
+  req.Header.Add("X-GitHub-Api-Version","2022-11-28")
+
+  resp, err := client.Do(req)
+
+  defer resp.Body.Close()
+
+  var events []Event 
+  resp_body, err := io.ReadAll(resp.Body)
+
+  json.Unmarshal(resp_body, &events)
+
+  for _, event := range events {
+    fmt.Print(event)
+  }
+}
 func main() {
   Execute()
 }
